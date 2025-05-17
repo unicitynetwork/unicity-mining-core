ac36078b71c7011fe04b7609af956f074d777985
user <makoto.sakuyama@proton.me>
2025-05-17 18:50:32 +0400

    removed gettransaction to remove local wallet requirement

--- src/Miningcore/Blockchain/Alpha/AlphaPayoutHandler.cs
+++ src/Miningcore/Blockchain/Alpha/AlphaPayoutHandler.cs
@@ -1,6 +1,7 @@
 using Autofac;
 using AutoMapper;
 using Miningcore.Blockchain.Bitcoin;
+using Miningcore.Blockchain.Bitcoin.DaemonResponses;
 using Miningcore.Configuration;
 using Miningcore.Extensions;
 using Miningcore.Messaging;
@@ -15,7 +16,9 @@ using Miningcore.Util;
 using Newtonsoft.Json;
 using NLog;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Contract = Miningcore.Contracts.Contract;
@@ -41,6 +44,125 @@ namespace Miningcore.Blockchain.Alpha
 
         protected override string LogCategory => "Alpha Payout Handler";
 
+        /// <summary>
+        /// Override ClassifyBlocksAsync to use blockchain-level commands instead of wallet commands
+        /// This allows block classification to work even when the wallet is on a different machine
+        /// </summary>
+        public override async Task<Persistence.Model.Block[]> ClassifyBlocksAsync(IMiningPool pool, Persistence.Model.Block[] blocks, CancellationToken ct)
+        {
+            Contract.RequiresNonNull(poolConfig);
+            Contract.RequiresNonNull(blocks);
+
+            if(blocks.Length == 0)
+                return blocks;
+
+            var coin = poolConfig.Template.As<CoinTemplate>();
+            var pageSize = 100;
+            var pageCount = (int) Math.Ceiling(blocks.Length / (double) pageSize);
+            var result = new List<Persistence.Model.Block>();
+
+            // Get current blockchain info for confirmation counting
+            var infoResponse = await rpcClient.ExecuteAsync<Miningcore.Blockchain.Bitcoin.DaemonResponses.BlockchainInfo>(logger, BitcoinCommands.GetBlockchainInfo, ct);
+            if(infoResponse.Error != null)
+            {
+                logger.Warn(() => $"[{LogCategory}] Error getting blockchain info: {infoResponse.Error.Message}");
+                return blocks;
+            }
+
+            var currentBlockHeight = infoResponse.Response.Blocks;
+
+            // Determine minimum confirmations required
+            var bitcoinTemplate = coin as BitcoinTemplate;
+            var minConfirmations = extraPoolEndpointConfig?.MinimumConfirmations ?? (bitcoinTemplate?.CoinbaseMinConfimations ?? BitcoinConstants.CoinbaseMinConfimations);
+            
+            for(var i = 0; i < pageCount; i++)
+            {
+                // Get a page full of blocks
+                var page = blocks
+                    .Skip(i * pageSize)
+                    .Take(pageSize)
+                    .ToArray();
+
+                for(var j = 0; j < page.Length; j++)
+                {
+                    var block = page[j];
+                    
+                    // Skip blocks that don't have hashes (shouldn't happen, but just in case)
+                    if(string.IsNullOrEmpty(block.Hash))
+                    {
+                        logger.Warn(() => $"[{LogCategory}] Block {block.BlockHeight} has no hash");
+                        continue;
+                    }
+
+                    // Get block information using the hash stored in our database
+                    // Use verbose=1 to get transaction IDs (we don't need full tx details)
+                    var blockResponse = await rpcClient.ExecuteAsync<Miningcore.Blockchain.Bitcoin.DaemonResponses.Block>(logger, BitcoinCommands.GetBlock, ct, new object[] { block.Hash, 1 });
+                    
+                    // If the daemon doesn't recognize the block hash, it's orphaned
+                    if(blockResponse.Error != null)
+                    {
+                        if(blockResponse.Error.Code == (int)BitcoinRPCErrorCode.RPC_INVALID_ADDRESS_OR_KEY)
+                        {
+                            block.Status = BlockStatus.Orphaned;
+                            block.Reward = 0;
+                            result.Add(block);
+                            
+                            logger.Info(() => $"[{LogCategory}] Block {block.BlockHeight} classified as orphaned - block hash not found in blockchain");
+                            
+                            messageBus.NotifyBlockUnlocked(poolConfig.Id, block, coin);
+                        }
+                        else
+                        {
+                            logger.Warn(() => $"[{LogCategory}] Error retrieving block {block.BlockHeight}: {blockResponse.Error.Message}");
+                        }
+                        
+                        continue;
+                    }
+                    
+                    var blockInfo = blockResponse.Response;
+                    
+                    // If the block has negative confirmations, it's orphaned
+                    if(blockInfo.Confirmations < 0)
+                    {
+                        block.Status = BlockStatus.Orphaned;
+                        block.Reward = 0;
+                        result.Add(block);
+                        
+                        logger.Info(() => $"[{LogCategory}] Block {block.BlockHeight} classified as orphaned - negative confirmations");
+                        
+                        messageBus.NotifyBlockUnlocked(poolConfig.Id, block, coin);
+                        continue;
+                    }
+                    
+                    // Check confirmation progress
+                    var confirmations = blockInfo.Confirmations;
+                    var confirmationProgress = Math.Min(1.0d, (double) confirmations / minConfirmations);
+                    
+                    // Update progress
+                    block.ConfirmationProgress = confirmationProgress;
+                    
+                    // Keep the original reward value from when the block was found
+                    // We can't directly get the reward from getblock as it doesn't provide that info
+                    result.Add(block);
+                    
+                    messageBus.NotifyBlockConfirmationProgress(poolConfig.Id, block, coin);
+                    
+                    // Check if the block is confirmed (matured)
+                    if(confirmations >= minConfirmations)
+                    {
+                        block.Status = BlockStatus.Confirmed;
+                        block.ConfirmationProgress = 1;
+                        
+                        logger.Info(() => $"[{LogCategory}] Unlocked block {block.BlockHeight} worth {FormatAmount(block.Reward)}");
+                        
+                        messageBus.NotifyBlockUnlocked(poolConfig.Id, block, coin);
+                    }
+                }
+            }
+            
+            return result.ToArray();
+        }
+        
         /// <summary>
         /// Override PayoutAsync to queue payments for external processing
         /// instead of sending them directly via the daemon.
